60147a8ba2d98cccd180ab778b404550a8cede82
iceBear67 <iceBear67@sfclub.cc>
2021-03-28 10:25:36 +0800

    Made ISender.waitChoice reactively

--- Core/src/main/java/cc/sfclub/packy/session/ISender.java
+++ Core/src/main/java/cc/sfclub/packy/session/ISender.java
@@ -4,6 +4,7 @@ import cc.sfclub.packy.repo.data.local.AbstractPackageInfo;
 import org.jetbrains.annotations.ApiStatus;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 @ApiStatus.AvailableSince("0.1.0")
 public interface ISender {
@@ -25,5 +26,9 @@ public interface ISender {
      *
      * @return result
      */
-    boolean waitChoice();
+    void waitChoice(Consumer<Choice> callback);
+
+    enum Choice {
+        TIMEOUT, YES, NO
+    }
 }
