9d8db880292cda82ea36df52ffbd99a4d76b32cd
iceBear67 <icebear67@sfclub.cc>
2021-10-10 21:03:01 +0800

    Instance Discovery

--- core-api/src/main/java/cc/sfclub/packy/api/IPacky.java
+++ core-api/src/main/java/cc/sfclub/packy/api/IPacky.java
@@ -1,4 +1,17 @@
 package cc.sfclub.packy.api;
 
+import cc.sfclub.packy.util.Lazy;
+
+import java.util.ServiceLoader;
+
 public interface IPacky {
+    static IPacky getInstance() {
+        return InstFinder.FINDER.get();
+    }
+
+    class InstFinder {
+        private static final Lazy<?, IPacky> FINDER = Lazy.by(() -> {
+            return ServiceLoader.load(IPacky.class).iterator().next();
+        });
+    }
 }
